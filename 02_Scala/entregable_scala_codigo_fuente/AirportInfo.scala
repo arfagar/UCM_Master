cb08266e2b162e391d70cc043bb14933ea2b89f0
arfagar <90319848+arfagar@users.noreply.github.com>
2023-12-06 15:13:03 +0100

    Add files via upload

---
+++ 02_Scala/entregable_scala_codigo_fuente/AirportInfo.scala
@@ -0,0 +1,8 @@
+package entregable.FlightsLoader
+
+case class AirportInfo(
+                        airportId: Long,
+                        code: String,
+                        cityName: String,
+                        stateAbr: String,
+                      )
